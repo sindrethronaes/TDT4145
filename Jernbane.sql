c6a218d6162f16aecf8dbcb264b5f9076ab77bce
Sigve Johan Hov <sigvejh@stud.ntnu.no>
2023-03-14 11:28:14 +0100

    Edit SQL syntax

--- Jernbane.sql
+++ Jernbane.sql
@@ -3,128 +3,118 @@
 DROP TABLE {Stasjon, DelstrekningIHovedretning, Togrute, TogruterPåBanestrekning, Operatører, Datoer, AntallVogntyper, Banestrekning, DelstrekningID, Vogn, Sovevogn, Sittevogn, Kunde, KundeOrdre, BillettISittevogn, BillettISovevogn, Rutestopp, Ordre, DatoerForTogruter}
 
 -- Lager DB
-CREATE TABLE "Stasjon" (
+
+CREATE TABLE Stasjon (
 	"StasjonNavn"	TEXT NOT NULL PRIMARY KEY,
 	"Moh."	INT NOT NULL
 );
 
-CREATE TABLE "DelstrekningIHovedretning" (
+CREATE TABLE DelstrekningIHovedretning (
 	"DelstrekningID" TEXT NOT NULL PRIMARY KEY,
-	"Startstasjon"	TEXT NOT NULL ,
-	"Endestasjon" TEXT NOT NULL ,
-	FOREIGN KEY(Endestasjon)REFERENCES stasjon(StasjonNavn),
-	FOREIGN KEY(Startstasjon)REFERENCES stasjon(StasjonNavn)
+	"Startstasjon"	TEXT NOT NULL REFERENCES Stasjon(StasjonNavn),
+	"Endestasjon" TEXT NOT NULL REFERENCES Stasjon(StasjonNavn)
 );
 
-CREATE TABLE "Togrute" (
-	"TogruteNavn"	TEXT NOT NULL PRIMARY KEY,
-	"DelstrekningID"	TEXT NOT NULL,
-	FOREIGN KEY(DelstrekningID)REFERENCES DelstrekningIHovedretning(DelstrekningID)
+CREATE TABLE Togrute (
+	"TogruteNavn"	TEXT PRIMARY KEY,
+	"DelstrekningID"	TEXT NOT NULL REFERENCES DelstrekningIHovedretning(DelstrekningID)
 );
 
 
-CREATE TABLE "TogruterPåBanestrekning" (
-	"Banestrekning"	TEXT NOT NULL PRIMARY KEY,
-	"TogruteNavn"	TEXT NOT NULL,
-	FOREIGN KEY(TogruteNavn)REFERENCES Togrute(TogruteNavn)
+CREATE TABLE TogruterPåBanestrekning (
+	"Banestrekning"	TEXT PRIMARY KEY,
+	"TogruteNavn"	TEXT NOT NULL REFERENCES Togrute(TogruteNavn)
 	);
 
-CREATE TABLE "Operatører" (
-	"OperatørerNavn"	TEXT NOT NULL PRIMARY KEY,
+CREATE TABLE Operatør (
+	"OperatørNavn"	TEXT PRIMARY KEY,
 	"Banestrekning"	TEXT NOT NULL REFERENCES TogruterPåBanestrekning(Banestrekning),
 	);
 
-CREATE TABLE "Datoer" (
-	"Dato" date NOT NULL PRIMARY KEY,
-	"Ukedag"	TEXT NOT NULL
+CREATE TABLE Dato (
+	"Dato" DATE NOT NULL PRIMARY KEY,
+	"Ukedag" TEXT NOT NULL
 	);
 
-CREATE TABLE "AntallVogntyper" (
-	"OperatørerNavn" PRIMARY KEY,
-	AntallVogntyper INT NOT NULL
+CREATE TABLE AntallVogntyper (
+	"OperatørNavn" PRIMARY KEY REFERENCES Operatør(OperatørNavn),
+	"AntallVogntyper" INT NOT NULL
 	);
 
-CREATE TABLE "Banestrekning" (
+CREATE TABLE Banestrekning (
 	"BanestrekningID" TEXT NOT NULL PRIMARY KEY,
 	"BanestrekningNavn"	TEXT NOT NULL,
 	"Fremdriftsenergi" TEXT NOT NULL
 	);
 
-CREATE TABLE "DeltrekningInfo" (
+CREATE TABLE Deltrekning (
 	"DelstrekningID" TEXT NOT NULL PRIMARY KEY,
 	"LengdeIkm"	INT NOT NULL,
 	"HarDobbbeltspor" TEXT NOT NULL
 	);
 
-CREATE TABLE "Vogn" (
+CREATE TABLE Vogn (
 	"VognID" TEXT NOT NULL PRIMARY KEY,
 	"Navn"	TEXT NOT NULL,
-	"OperatørerNavn" TEXT NOT NULL,
 	"TilgjengeligForBruk" TEXT NOT NULL,
 	"NummerIVognsammensetning" INT NOT NULL,
 	"VognType" TEXT NOT NULL,
-	FOREIGN KEY (OperatørerNavn) REFERENCES Operatører(OperatørerNavn)
+	"OperatørerNavn" TEXT NOT NULL REFERENCES Operatør(OperatørerNavn)
 	);
 
-CREATE TABLE "Sovevogn" (
-	"VognID" TEXT NOT NULL PRIMARY KEY,
+CREATE TABLE Sovevogn (
+	"VognID" TEXT PRIMARY KEY REFERENCES Vogn(VognID),
 	"Seng"	TEXT NOT NULL
 	);
 
-CREATE TABLE "Sittevogn" (
-	"VognID" TEXT NOT NULL PRIMARY KEY,
+CREATE TABLE Sittevogn (
+	"VognID" TEXT PRIMARY KEY REFERENCES Vogn(VognID),
 	"Sete"	TEXT NOT NULL
 	);
 
 
-CREATE TABLE "Kunde" (
-	"KundeID" TEXT NOT NULL PRIMARY KEY,
+CREATE TABLE Kunde (
+	"KundeID" TEXT PRIMARY KEY,
 	"Navn"	TEXT NOT NULL,
-	"E-post" TEXT NOT NULL,
-	"Number" INT NOT NULL
+	"Epost" TEXT NOT NULL,
+	"Nummer" INT NOT NULL
 	);
 
-CREATE TABLE "KundeOrdre" (
+CREATE TABLE KundeOrdre (
 	"OrdreID" TEXT NOT NULL PRIMARY KEY,
-	"Dato"	date NOT NULL,
-	"TogruteNavn" TEXT NOT NULL,
-	"KundeID" TEXT NOT NULL,
-	FOREIGN KEY (TogruteNavn) REFERENCES Togrute(TogruteNavn),
-	FOREIGN KEY (KundeID) REFERENCES Kunde(KundeID)
+	"Dato"	DATE NOT NULL,
+	"TogruteNavn" TEXT NOT NULL REFERENCES Togrute(TogruteNavn),
+	"KundeID" TEXT NOT NULL REFERENCES Kunde(KundeID)
 	);
 
-CREATE TABLE "BillettISittevogn" (
+CREATE TABLE BillettISittevogn (
 	"BillettID" TEXT NOT NULL PRIMARY KEY,
-	"Sete" TEXT NOT NULL,
-	FOREIGN KEY (Sete) REFERENCES Sittevogn(Sete)
+	"Sete" TEXT NOT NULL REFERENCES Sittevogn(Sete)
 	);
 
-CREATE TABLE "BillettISovevogn" (
+CREATE TABLE BillettISovevogn (
 	"BillettID" TEXT NOT NULL PRIMARY KEY,
-	"Seng" TEXT NOT NULL,
-	FOREIGN KEY (Seng) REFERENCES Sovevogn(Seng)
+	"Seng" TEXT NOT NULL REFERENCES Sovevogn(Seng)
 	);
 
 
 
-CREATE TABLE "Rutestopp" (
-	"TogruteNavn" TEXT NOT NULL,
-	"StasjonNavn" TEXT NOT NULL,
+CREATE TABLE Rutestopp (
+	"TogruteNavn" TEXT PRIMARY KEY REFERENCES Togrute(TogruteNavn),
+	"StasjonNavn" TEXT PRIMARY KEY REFERENCES Stasjon(StasjonNavn),
 	"Avgang" time NOT NULL,
 	"Ankomst" time NOT NULL,
-	PRIMARY KEY(TogruteNavn, StasjonNavn)
 	);
 
 
-CREATE TABLE "Ordre" (
-	"OrdreID" TEXT NOT NULL PRIMARY KEY,
+CREATE TABLE Ordre (
+	"OrdreID" TEXT PRIMARY KEY,
 	"BillettID" TEXT NOT NULL
 	);
 
 
-CREATE TABLE "DatoerForTogruter" (
-	"Togrutenavn" TEXT NOT NULL PRIMARY KEY,
-	"Dato" date NOT NULL,
-	FOREIGN KEY (Dato) REFERENCES Datoer(Dato)
+CREATE TABLE DatoerForTogruter (
+	"Togrutenavn" TEXT PRIMARY KEY REFERENCES Togrute(TogruteNavn),
+	"Dato" DATE NOT NULL REFERENCES Dato(Dato)
 	);
 
--- initDB.py
+++ initDB.py
@@ -2,24 +2,13 @@ import sqlite3
 
 # Creates a connection to out database
 con = sqlite3.connect("TogDB.db")
-# Creates a connecting object to our database
-cursor = con.cursor()
 
-cursor.executescript("Jernbane.sql")
+# Read SQL script from file
+with open('Jernbane.sql', 'r') as f:
+    sql_script = f.read()
 
-"""
-# Lager tabellen Banestrekning
-cursor.execute('''CREATE TABLE Banestrekning (BanestrekningID TEXT NOT NULL PRIMARY KEY, BanestrekningNavn	TEXT NOT NULL, Fremdriftsenergi TEXT NOT NULL)''')
 
-# Lager tabellen stasjon
-cursor.execute('''CREATE TABLE Stasjon ("StasjonNavn"	TEXT NOT NULL PRIMARY KEY, "Moh"	FLOAT NOT NULL)''')
+con.executescript(sql_script)
 
-# Lager tabellen Delstrekning
-cursor.execute('''CREATE TABLE Delstrekning("DelstrekningID"	INT NOT NULL PRIMARY KEY, "LengdeIKM" INT NOT NULL, "HarDobbeltSpor" BOOLEAN NOT NULL, "BanestrekningID"	INT NOT NULL REFERENCES Banestrekning(BanestrekningID), "StartStasjon" TEXT NOT NULL REFERENCES Stasjon(StasjonNavn), "EndeStasjon" TEXT NOT NULL REFERENCES Stasjon(StasjonNavn))''')
-
-# Creates table Togrute
-cursor.execute('''CREATE TABLE Togrute ("TogruteNavn"	TEXT NOT NULL PRIMARY KEY, "Avgangstid" DATETIME NOT NULL, "Ankomststid" DATETIME NOT NULL, "Startstasjon"	TEXT NOT NULL REFERENCES Stasjon(StasjonNavn), "Endestasjon" TEXT NOT NULL REFERENCES Stasjon(StasjonNavn))''')
-
-"""
-
-cursor.connection.commit()
+con.commit()
+con.close()
