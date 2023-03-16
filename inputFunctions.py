ff4defb7cb48f1b2ba33587b9db4c3b0c298e962
Hanne Fyrand <hannelf@stud.ntnu.no>
2023-03-16 10:30:23 +0100

    endring

---
+++ inputFunctions.py
@@ -0,0 +1,7 @@
+def get_station_name():
+    station_name = input("Enter station name: ")
+    return station_name
+    
+def get_weekday():
+    weekday = input("Enter weekday number (e.g., Sunday = 0, monday = 1 etc.): ")
+    return weekday
